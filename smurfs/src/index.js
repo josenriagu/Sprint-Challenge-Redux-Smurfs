b89d3e088ba9508f9a09f1eabbf1646846666671
Josemaria <josemarianriagu25@gmail.com>
2019-06-21 17:26:12 +0800

    Add data persistence to Redux store

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,13 +6,24 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import rootreducer from './reducers';
+import { loadState, saveState } from './localStorage';
 import App from './App';
 import './index.css';
 
+const persistedState = loadState();
+
 const store = createStore(
   rootreducer,
+  persistedState,
   applyMiddleware(thunk, logger)
 );
+
+// save only smurf slice of state
+store.subscribe(() => {
+  saveState({
+    smurfs: store.getState().smurfs
+  });
+});
 
 ReactDOM.render(
   <Provider store={store}>
